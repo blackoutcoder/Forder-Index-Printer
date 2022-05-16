f60cb8840aaab2372dcc2efbd7d3f7d43206017f
blackoutcoder <94862044+blackoutcoder@users.noreply.github.com>
2022-05-16 20:16:53 +0300

    Add project files.

---
+++ Forder Index Printer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Forder_Index_Printer
+{
+    internal class Program
+    {
+        public string path;
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine($"Files in folder ");
+            while (true)
+            {
+                PrintFilesFromFolder();
+                await Task.Delay(5000);
+            }
+            
+        }
+
+        static void PrintFilesFromFolder()
+        {
+            string path = @"C:\";
+            string[] files = Directory.GetFiles(path);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine(files[i]);
+            }
+        }
+    }
+}
